48371b01d15e0c00785d0764b5dc02125ec885e0
Marc Costafreda Escofet <marccostafredaescofet@Marcs-Mac-mini.local>
2024-09-18 17:35:19 +0200

    First draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+
+
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), icon()],
+  site: "http://localhost:4321",
+  integrations: [tailwind(), icon(), react()],
 });
